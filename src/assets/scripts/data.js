d4138206bcba8712c1e70fa9565edfd48d138c87
RosaBen <benchabane.rosa@outlook.com>
2026-07-20 20:34:48 +0200

    added cards for courses with css

--- src/assets/scripts/data.js
+++ src/assets/scripts/data.js
@@ -61,38 +61,47 @@ export const projects = [
 // My courses
 export const courses = [
   {
-    logo: oclock,
-    name: "O'Clock",
-    description: "C'était ma toute première formation intensive. J'ai appris les basiques comme HTML, CSS, Javascript, Bootstrap. J'ai ensuite choisi le module PHP pour le reste de la formation. C'est une très bonne école, mais je n'arrivais pas à suivre parce que c'était intensif",
-    skills: ["HTML&CSS", "Javascript", "Bootstrap", "API", "PHP", "Symfony"],
-    date: "mai 2023 - octobre 2023"
-  },
-  {
-    logo: wcs,
-    name: "Wild Code School",
-    description: "C'était ma seconde école. J'ai perfectionné les basiques comme HTML, CSS, Javascript, Bootstrap. J'ai aussi appris React. C'est une aussi très bonne école, j'ai aussi avancé avec cette école, mieux compris certaines choses",
-    skills: ["HTML&CSS", "Javascript", "Bootstrap", "API", "React", "Node.js"],
-    date: "mai 2024 - octobre 2024"
-  },
-  {
-    logo: thp,
-    name: "The Hacking Project",
-    description: "C'était ma 3èem école. J'ai encore perfectionné les basiques et j'ai appris un nouveau language : Ruby. J'ai fait du back end avec Rails 8. J'ai beaucoup aimé Ruby. C'est une école en peer to peer, on se corrigeait entre nous. J'ai appris un peu plus sur le web3",
-    skills: ["HTML&CSS", "Javascript", "Bootstrap", "API", "Ruby", "Rails8"],
-    date: "mai 2025 - octobre 2025"
+    logo: scrimba,
+    name: "Scrimba",
+    description: "C'est un site en ligne pour apprendre pas à pas la programmation web et mobile. J'aime beaucoup les cours qui sont très bien expliqué avec des minis challenges corrigés par IA. Je recommande très fortement ce site où j'ai pu comprendre pas mal de choses en pratiquant",
+    skills: ["HTML&CSS", "Javascript", "React"],
+    date: "octobre 2025 - en cours",
+    url: "https://scrimba.com/"
   },
   {
     logo: cef,
     name: "Centre Européen de Formation",
     description: "C'est mon école en ce moment, nous apprenons à notre rythme avec les ressources proposées. Je suis sur le module React que je trouve très léger comparé aux autre écoles. Pour le moment, j'arrive à faire mes devoirs. ",
     skills: ["HTML&CSS", "Javascript", "Bootstrap"],
-    date: "mars 2026 - en cours"
+    date: "mars 2026 - en cours",
+    url: "https://www.centre-europeen-formation.fr/"
   },
   {
-    logo: scrimba,
-    name: "Scrimba",
-    description: "C'est un site en ligne pour apprendre pas à pas la programmation web et mobile. J'aime beaucoup les cours qui sont très bien expliqué avec des minis challenges corrigés par IA. Je recommande très fortement ce site où j'ai pu comprendre pas mal de choses en pratiquant",
-    skills: ["HTML&CSS", "Javascript", "React"],
-    date: "octobre 2025 - en cours"
+    logo: thp,
+    name: "The Hacking Project",
+    description: "C'était ma 3èem école. J'ai encore perfectionné les basiques et j'ai appris un nouveau language : Ruby. J'ai fait du back end avec Rails 8. J'ai beaucoup aimé Ruby. C'est une école en peer to peer, on se corrigeait entre nous. J'ai appris un peu plus sur le web3",
+    skills: ["HTML&CSS", "Javascript", "Bootstrap", "API", "Ruby", "Rails8"],
+    date: "mai 2025 - octobre 2025",
+    url: "https://thehackingproject.org/"
   },
+  {
+    logo: wcs,
+    name: "Wild Code School",
+    description: "C'était ma seconde école. J'ai perfectionné les basiques comme HTML, CSS, Javascript, Bootstrap. J'ai aussi appris React. C'est une aussi très bonne école, j'ai aussi avancé avec cette école, mieux compris certaines choses",
+    skills: ["HTML&CSS", "Javascript", "Bootstrap", "API", "React", "Node.js"],
+    date: "mai 2024 - octobre 2024",
+    url: "https://www.wildcodeschool.com/"
+  },
+  {
+    logo: oclock,
+    name: "O'Clock",
+    description: "C'était ma toute première formation intensive. J'ai appris les basiques comme HTML, CSS, Javascript, Bootstrap. J'ai ensuite choisi le module PHP pour le reste de la formation. C'est une très bonne école, mais je n'arrivais pas à suivre parce que c'était intensif",
+    skills: ["HTML&CSS", "Javascript", "Bootstrap", "API", "PHP", "Symfony"],
+    date: "mai 2023 - octobre 2023",
+    url: "https://oclock.io/"
+  },
+
+
+
+
 ];
